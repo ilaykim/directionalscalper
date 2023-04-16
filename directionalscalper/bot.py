54c4b2e918a2c1418667cdf39579302bdef57bb5
donewiththedollar <donewiththedollar@gmail.com>
2023-04-16 20:14:00 +0000

    Argparse and no config exception handling

--- directionalscalper/bot.py
+++ directionalscalper/bot.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import time
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -23,7 +24,6 @@ from directionalscalper.strategy.scalein import Scalein
 from directionalscalper.strategy.short import Short
 from directionalscalper.strategy.violent import Violent
 
-
 class Bot:
     def __init__(self, api, exchange, config, strategy):
         self.api = api
@@ -32,9 +32,14 @@ class Bot:
         self.strategy = strategy
         self.version = "1.1.7"
 
-        self.quote = "USDT"
-        self.exchange.setup_exchange(symbol=self.config.symbol)
-        self.run()
+        self.startup_message()
+
+        if self.exchange is not None and self.config is not None:
+            self.quote = "USDT"
+            self.exchange.setup_exchange(symbol=self.config.symbol)
+            self.run()
+        else:
+            log.error("Unable to start the bot without a valid configuration.")
 
     def run(self):
         balance = self.exchange.get_balance(quote=self.quote)
@@ -44,13 +49,44 @@ class Bot:
         log.info(ob)
         log.info(positions)
 
+    def startup_message(self):
+        log.info(f"Loading {self.strategy.get_name()} strategy, version {self.strategy.get_version()}")
+
 
 if __name__ == "__main__":
-    config_file = "config.json"
-    config_file_path = Path(Path().resolve(), "config", config_file)
-    config = load_config(path=config_file_path)
+    parser = argparse.ArgumentParser(description="Directional Scalper Bot")
+    parser.add_argument("--config", default="config.json", help="Configuration file")
+    parser.add_argument("--strategy", default="hedge", help="Trading strategy")
+    args = parser.parse_args()
 
+    config_file = args.config
+    config_file_path = Path(Path().resolve(), "config", config_file)
+    strategy_name = args.strategy.lower()
+
+    # Check if config file exists
+    if not config_file_path.exists():
+        print(f"Error: Configuration file '{config_file_path}' does not exist.")
+        exit(1)
+
+    config = load_config(path=config_file_path)
     log = Logger(filename="ds.log", level=config.logger.level)
+
+    # Check for a valid strategy
+    strategy_map = {
+        "aggressive": Aggressive,
+        "blackjack": Blackjack,
+        "hedge": Hedge,
+        "long": Long,
+        "scalein": Scalein,
+        "short": Short,
+        "violent": Violent,
+    }
+
+    if strategy_name in strategy_map:
+        strategy = strategy_map[strategy_name]()
+    else:
+        log.error(f"Invalid strategy: {strategy_name}. Please choose a valid strategy.")
+        exit(1)
 
     manager = Manager(
         api=config.api.mode,
@@ -59,6 +95,4 @@ if __name__ == "__main__":
     )
 
     exchange = Exchange(exchange_name=config.exchange.name, config=config.exchange)
-    strategy = Violent()
-
     bot = Bot(exchange=exchange, api=manager, config=config.bot, strategy=strategy)
