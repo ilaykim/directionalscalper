baaa4a2cf4531d11a9ea50df1c038cd7d2ea575c
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-04-17 19:51:15 +0100

    base functions

--- directionalscalper/strategy/strategy.py
+++ directionalscalper/strategy/strategy.py
@@ -5,14 +5,23 @@ log = logging.getLogger(__name__)
 
 class Strategy:
     def __init__(self):
-        self.name = None
-        self.version = None
+        self.name = "Not loaded"
+        self.version = "0.0.0"
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
 
-    def get_version(self):
+    def get_version(self) -> str:
         return self.version
 
     def startup_message(self):
         log.info(f"Loading {self.name} strategy, version {self.version}")
+
+    def position_entry(self):
+        pass
+
+    def position_exit(self):
+        pass
+
+    def check_orders(self):
+        pass
